c2e452a3d6995a333ba98004b9cdffc03aa1d901
coderrick <derrick.duncan@temple.edu>
2017-04-23 01:57:40 -0400

    Merged latest

--- pyosc_server.py
+++ pyosc_server.py
@@ -4,26 +4,16 @@ import math
 from pythonosc import dispatcher
 from pythonosc import osc_server
 
+def xdata_handler(unused_addr,xdata):
+  print("x=", xdata)
 
-def eeg_handler(unused_addr, args, ch1, ch2, ch3, ch4):
-    print("EEG (uV) per channel: ", ch1, ch2, ch3, ch4)
+def ydata_handler(unused_addr,ydata):
+  print("y=", ydata)
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--ip",
-                        default="127.0.0.1",
-                        help="The ip to listen on")
-    parser.add_argument("--port",
-                        type=int,
-                        default=5005,
-                        help="The port to listen on")
-    args = parser.parse_args()
-
-    dispatcher = dispatcher.Dispatcher()
-    dispatcher.map("/debug", print)
-    dispatcher.map("/muse/eeg", eeg_handler, "EEG")
-
-    server = osc_server.ThreadingOSCUDPServer(
-        (args.ip, args.port), dispatcher)
-    print("Serving on {}".format(server.server_address))
-    server.serve_forever()
+dispatcher = dispatcher.Dispatcher()
+dispatcher.map("/xdata", xdata_handler)
+dispatcher.map("/ydata", ydata_handler)
+server = osc_server.ThreadingOSCUDPServer(
+         ('localhost', 5000), dispatcher)
+server.serve_forever()
+print("HEy")
